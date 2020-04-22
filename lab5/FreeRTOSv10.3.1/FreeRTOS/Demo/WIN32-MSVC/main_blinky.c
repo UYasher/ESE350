09e90712e3b4234c1805e0cf34fb4128704b861a
SHRIYASH UPADHYAY <shriyash@park.key.seas.upenn.edu>
2020-04-21 21:57:19 -0400

    Fixed race conditions

--- lab5/FreeRTOSv10.3.1/FreeRTOS/Demo/WIN32-MSVC/main_blinky.c
+++ lab5/FreeRTOSv10.3.1/FreeRTOS/Demo/WIN32-MSVC/main_blinky.c
@@ -9,9 +9,7 @@
 #define INCLUDE_vTaskDelay                      1
 #define INCLUDE_xTaskGetCurrentTaskHandle       1
 // The tasks as described in the comments at the top of this file.
-static SemaphoreHandle_t printLock;
-static SemaphoreHandle_t chrisAwakeLock;
-static SemaphoreHandle_t queueLock;
+static SemaphoreHandle_t printLock, chrisAwakeLock, queueLock;
 static QueueHandle_t OHqueue;
 static const int queueSize = 5;
 static TaskHandle_t chrisHandle;
@@ -24,7 +22,7 @@ static void concurrency_safe_print(char* strToPrint) {
 	xSemaphoreGive(printLock);
 }
 
-static void student(char* name) {
+static void student() {
 	while (1) {
 		int wasHelped = 0;
 		xSemaphoreTake(chrisAwakeLock, portMAX_DELAY);
@@ -32,7 +30,7 @@ static void student(char* name) {
 			chrisAwake = 1;
 			vTaskResume(chrisHandle);
 			wasHelped = 2; // Make sure the student helped by Chris does not sleep eternally
-			concurrency_safe_print(name);
+			concurrency_safe_print(pcTaskGetName(xTaskGetCurrentTaskHandle()));
 			concurrency_safe_print(" went from going about life to being helped by Chris\n");
 		}
 		else {
@@ -49,20 +47,18 @@ static void student(char* name) {
 		if (wasHelped) {
 			// Suspend thread if waiting in queue
 			if (wasHelped == 1) {
-				concurrency_safe_print(name);
+				concurrency_safe_print(pcTaskGetName(xTaskGetCurrentTaskHandle()));
 				concurrency_safe_print(" went from going about life to waiting in OH\n");
 				vTaskSuspend(xTaskGetCurrentTaskHandle());
-				concurrency_safe_print(name);
-				concurrency_safe_print(" went from waiting in OH to being helped by Chris\n");
 			}
-			concurrency_safe_print(name);
+			concurrency_safe_print(pcTaskGetName(xTaskGetCurrentTaskHandle()));
 			concurrency_safe_print(" went from being helped by Chris to going about life\n");
 			// Wait if helped
 			int delay_time = rand() % 5 + 3;
 			vTaskDelay(delay_time * second);
 		}
 		else {
-			concurrency_safe_print(name);
+			concurrency_safe_print(pcTaskGetName(xTaskGetCurrentTaskHandle()));
 			concurrency_safe_print(" went from going about life to going about life\n");
 			// Wait if not helped
 			int delay_time = rand() % 5 + 1;
@@ -76,26 +72,27 @@ static void chris() {
 	chrisHandle = xTaskGetCurrentTaskHandle();
 
 	while (1) {
-		int queueEmpty = 0;
+		xSemaphoreTake(chrisAwakeLock, portMAX_DELAY);
 		xSemaphoreTake(queueLock, portMAX_DELAY);
-		if (uxQueueSpacesAvailable(OHqueue) - queueSize <= 0) {
+		if (uxQueueSpacesAvailable(OHqueue) - queueSize == 0) { // If there is nobody on the queue
 			concurrency_safe_print("QUEUE EMPTY\n");
-			queueEmpty = 1;
+			chrisAwake = 0;
 		}
 		else {
+			concurrency_safe_print("Chris went from helping students to helping students\n");
 			// Create Temp TaskHandle
 			TaskHandle_t temp = NULL;
 			// Load student from queue into buffer
 			xQueueReceive(OHqueue, &(temp), 0);
 			// Wake up Student
+			concurrency_safe_print(pcTaskGetName(temp));
+			concurrency_safe_print(" went from waiting in OH to being helped by Chris\n");
 			vTaskResume(temp);
 		}
 		xSemaphoreGive(queueLock);
-		if (queueEmpty) {
-			xSemaphoreTake(chrisAwakeLock, portMAX_DELAY);
-			chrisAwake = 0;
+		xSemaphoreGive(chrisAwakeLock);
+		if (chrisAwake == 0) {
 			concurrency_safe_print("Chris went from helping students to playing Sudoku\n");
-			xSemaphoreGive(chrisAwakeLock);
 			vTaskSuspend(chrisHandle);
 			concurrency_safe_print("Chris went from playing Sudoku to helping students\n");
 		}
@@ -108,22 +105,10 @@ static void chrisTask(void *pvParameters)
 	chris();
 }
 
-static void shriyash(void *pvParameters)
+static void studentTask(void *pvParameters)
 {
 	(void)pvParameters;
-	student("Shriyash");
-}
-
-static void richard(void *pvParameters)
-{
-	(void)pvParameters;
-	student("Richard");
-}
-
-static void lakshay(void *pvParameters)
-{
-	(void)pvParameters;
-	student("Lakshay");
+	student();
 }
 
 void main_blinky(void)
@@ -132,13 +117,28 @@ void main_blinky(void)
 	chrisAwakeLock = xSemaphoreCreateMutex();
 	queueLock = xSemaphoreCreateMutex();
 	OHqueue = xQueueCreate(queueSize, sizeof(TaskHandle_t));
+	
+	char studentNames[10][12] = {
+		"Shriyash",
+		"Richard",
+		"Lakshay",
+		"Isaac",
+		"Neil",
+		"Katherine",
+		"Anshul",
+		"William",
+		"Peter",
+		"Micah"
+	};
 
 	if ((printLock != NULL) && (chrisAwakeLock != NULL) && (queueLock != NULL) && (OHqueue != NULL))
 	{
 		xTaskCreate(chrisTask, "Chris", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
-		xTaskCreate(shriyash, "Shriyash", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
-		xTaskCreate(richard, "Richard", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
-		xTaskCreate(lakshay, "Lakshay", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
+		
+		for (int i = 0; i < 10; i++) {
+			xTaskCreate(studentTask, studentNames[i], configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
+		}
+
 		vTaskStartScheduler();
 	}
 	printf("ERROR: Should not get here\r\n");
